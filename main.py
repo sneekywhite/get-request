1f776118c173f4ec4817fef0dffe86ef08ceaed0
sneekywhite <okoh.onyeka123@gmail.com>
2023-09-09 17:05:47 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from datetime import timedelta, datetime
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.encoders import jsonable_encoder
+
+
+app = FastAPI()
+
+
+@app.get('/api', tags=['GET REQUEST'])
+async def get_request(slack_name: str = Query(..., description="Your Slack name"),
+    track: str = Query(..., description="Your chosen track")):
+
+     # Get current day of the week
+    current_day = datetime.utcnow().strftime('%A')
+
+    # Get current UTC time
+    current_time = datetime.utcnow()
+    utc_time = current_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Validate UTC time within +/-2 minutes
+    two_minutes_ago = current_time - timedelta(minutes=2)
+    two_minutes_later = current_time + timedelta(minutes=2)
+    if not (two_minutes_ago <= current_time <= two_minutes_later):
+        raise HTTPException(status_code=400, detail="UTC time validation failed")
+
+    # Construct GitHub URLs (replace with your actual URLs)
+    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
+    github_repo_url = "https://github.com/username/repo"
+
+
+    response = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    
+    return jsonable_encoder(response)
+
+    
